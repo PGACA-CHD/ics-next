c0815eace4844e5a55f7d6928e15a643340f4cce
PGACA-CHD <pankaj@pgaca.in>
2026-03-16 19:56:27 +0530

    SEO: fix titles, add OG image + schema to about

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,6 +2,7 @@ import ClientPage from './client';
 
 export const dynamic = 'force-dynamic';
 
+
 export const metadata = {
   title: 'About India Company Setup | Ex-KPMG CA Team',
   description: "India Company Setup is an Ex-KPMG-led team of CAs, Company Secretaries, accountants and lawyers. 100+ foreign companies incorporated. 18+ years combined experience.",
@@ -28,7 +29,7 @@ export const schemaJson = JSON.stringify([{
       "name": "About India Company Setup | Ex-KPMG CA Team",
       "description": "India Company Setup is an Ex-KPMG-led team of CAs, Company Secretaries, accountants and lawyers. 100+ foreign companies incorporated. 18+ years combined experience.",
       "url": "https://www.indiacompanysetup.com/about",
-      "provider": {"@type": "Organization","@id": "https://www.indiacompanysetup.com/#organization","name": "India Company Setup"},
+      "provider": {"@type": "Organization", "@id": "https://www.indiacompanysetup.com/#organization", "name": "India Company Setup"},
       "areaServed": ["US", "GB", "AE", "SG", "AU"],
       "serviceType": "Business Advisory"
     },{
@@ -42,7 +43,7 @@ export const schemaJson = JSON.stringify([{
       "description": "End-to-end company setup in India for foreign businesses. Ex-KPMG led CA team. 100+ foreign companies incorporated.",
       "telephone": "+91-99157-31447",
       "email": "info@indiacompanysetup.com",
-      "address": {"@type": "PostalAddress","streetAddress": "SCO 18, Top Floor, Sector 20-D","addressLocality": "Chandigarh","postalCode": "160020","addressCountry": "IN"},
+      "address": {"@type": "PostalAddress", "streetAddress": "SCO 18, Top Floor, Sector 20-D", "addressLocality": "Chandigarh", "postalCode": "160020", "addressCountry": "IN"},
       "areaServed": ["US", "GB", "AE", "SG", "AU", "IN"],
       "priceRange": "$$"
     }]);
