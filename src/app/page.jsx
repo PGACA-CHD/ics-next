902a9297540a69c390b461d64076fba8b4d03531
PGACA-CHD <pankaj@pgaca.in>
2026-03-16 19:56:22 +0530

    SEO: fix titles, add OG image + schema to page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
-import ClientPage from './client';
+import ClientPage from './home-client';
 
 export const dynamic = 'force-dynamic';
+
 
 export const metadata = {
   title: 'Company Incorporation in India for Foreign Companies',
@@ -33,7 +34,7 @@ export const schemaJson = JSON.stringify([{
       "description": "End-to-end company setup in India for foreign businesses. Ex-KPMG led CA team. 100+ foreign companies incorporated.",
       "telephone": "+91-99157-31447",
       "email": "info@indiacompanysetup.com",
-      "address": {"@type": "PostalAddress","streetAddress": "SCO 18, Top Floor, Sector 20-D","addressLocality": "Chandigarh","postalCode": "160020","addressCountry": "IN"},
+      "address": {"@type": "PostalAddress", "streetAddress": "SCO 18, Top Floor, Sector 20-D", "addressLocality": "Chandigarh", "postalCode": "160020", "addressCountry": "IN"},
       "areaServed": ["US", "GB", "AE", "SG", "AU", "IN"],
       "priceRange": "$$"
     }]);
