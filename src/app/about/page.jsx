fbb53e62ee6b96aa28922e064a19e4a39fbc3b1e
PGACA-CHD <pankaj@pgaca.in>
2026-04-21 20:05:25 +0530

    Update page.jsx

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,16 +1,12 @@
 import ClientPage from './client';
 
-
-
-
-
 export const metadata = {
   title: 'About India Company Setup | Ex-Big 4 CA Team',
-  description: "India Company Setup is an Ex-Big 4 led team of CAs, Company Secretaries, accountants and lawyers. 100+ foreign companies incorporated. 18+ years combined experience.",
+  description: "India Company Setup is an Ex-Big 4 led team of CAs, Company Secretaries, accountants and lawyers. 100+ foreign companies incorporated.",
   alternates: { canonical: 'https://www.indiacompanysetup.com/about' },
   openGraph: {
     title: 'About India Company Setup | Ex-Big 4 CA Team',
-    description: "India Company Setup is an Ex-Big 4 led team of CAs, Company Secretaries, accountants and lawyers. 100+ foreign companies incorporated. 18+ years combined experience.",
+    description: "India Company Setup is an Ex-Big 4 led team of CAs, Company Secretaries, accountants and lawyers. 100+ foreign companies incorporated.",
     url: 'https://www.indiacompanysetup.com/about',
     images: [{ url: 'https://www.indiacompanysetup.com/og-image.jpg', width: 1200, height: 630, alt: 'India Company Setup' }],
     type: 'website',
@@ -19,20 +15,41 @@ export const metadata = {
   twitter: {
     card: 'summary_large_image',
     title: 'About India Company Setup | Ex-Big 4 CA Team',
-    description: "India Company Setup is an Ex-Big 4 led team of CAs, Company Secretaries, accountants and lawyers. 100+ foreign companies incorporated. 18+ years combined experience.",
+    description: "India Company Setup is an Ex-Big 4 led team of CAs, Company Secretaries, accountants and lawyers. 100+ foreign companies incorporated.",
     images: ['https://www.indiacompanysetup.com/og-image.jpg'],
   },
 };
 
-
 export const schemaJson = JSON.stringify([{
-      "@context": "https://schema.org",
-      "@type": "Service",
-      "name": "About India Company Setup | Ex-Big 4 CA Team",
-      "description": "India Company Setup is an Ex-Big 4 led team of CAs, Company Secretaries, accountants and lawyers. 100+ foreign companies incorporated. 18+ years combined experience.",
-      "url": "https://www.indiacompanysetup.com/about",
-      "provider": {"@type": "Organization", "@id": "https://www.indiacompanysetup.com/#organization", "name": "India Company Setup"},
-      "areaServed": ["US", "GB", "AE", "SG", "AU"],
-      "serviceType": "Business Advisory"
-    },{
-      "@context": "https://schema.org",
+  "@context": "https://schema.org",
+  "@type": "Service",
+  "name": "About India Company Setup | Ex-Big 4 CA Team",
+  "description": "India Company Setup is an Ex-Big 4 led team of CAs, Company Secretaries, accountants and lawyers. 100+ foreign companies incorporated.",
+  "url": "https://www.indiacompanysetup.com/about",
+  "provider": {"@type": "Organization", "@id": "https://www.indiacompanysetup.com/#organization", "name": "India Company Setup"},
+  "areaServed": ["US", "GB", "AE", "SG", "AU"],
+  "serviceType": "Business Advisory"
+},{
+  "@context": "https://schema.org",
+  "@type": ["Organization", "ProfessionalService"],
+  "@id": "https://www.indiacompanysetup.com/#organization",
+  "name": "India Company Setup",
+  "url": "https://www.indiacompanysetup.com",
+  "logo": "https://www.indiacompanysetup.com/og-image.jpg",
+  "image": "https://www.indiacompanysetup.com/og-image.jpg",
+  "description": "End-to-end company setup in India for foreign businesses. Ex-Big 4 led CA team. 100+ foreign companies incorporated.",
+  "telephone": "+91-99157-31447",
+  "email": "info@indiacompanysetup.com",
+  "address": {"@type": "PostalAddress", "streetAddress": "SCO 18, Top Floor, Sector 20-D", "addressLocality": "Chandigarh", "postalCode": "160020", "addressCountry": "IN"},
+  "areaServed": ["US", "GB", "AE", "SG", "AU", "IN"],
+  "priceRange": "$$"
+}]);
+
+export default function Page() {
+  return (
+    <>
+      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: schemaJson }} />
+      <ClientPage />
+    </>
+  );
+}
