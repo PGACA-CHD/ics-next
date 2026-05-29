56652b99735f0e25be488c34c87020aeeb1ee3c8
ICS <info@indiacompanysetup.com>
2026-05-29 17:52:37 +0530

    Update page.jsx

--- src/app/uk-company-setting-up-india/page.jsx
+++ src/app/uk-company-setting-up-india/page.jsx
@@ -1,6 +1,6 @@
 import CountryClientPage from '@/components/seo/CountryClient';
 
-export const dynamic = 'force-dynamic';
+export const revalidate = 3600;
 
 export const metadata = {
   title: 'UK Company Setting Up in India | India Subsidiary for UK Firms',
