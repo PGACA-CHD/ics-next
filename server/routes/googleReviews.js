aa317c969ea8384bf4ab5e232d5ac172d9836a83
SathishS24d <sathish@dkcomputers.in>
2026-07-07 22:24:24 +0530

    removed reviews

--- server/routes/googleReviews.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { getReviews } from '../controllers/googleReviewsController.js';
-
-const router = express.Router();
-router.get('/', getReviews);
-export default router;
